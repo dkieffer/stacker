ec589499af1070c0b721ace8e86be7d54ead7efa
David <david@kiefferbros.com>
2020-04-26 14:58:34 -0500

    build renamed to docs
    to deploy to gh pages

---
+++ docs/precache-manifest.5d23da4152ab988331aba4b151106bb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be628aec387a56d79a008475c2504bf5",
+    "url": "/dkieffer/stacker/index.html"
+  },
+  {
+    "revision": "f0bd0817acb14d1c1e95",
+    "url": "/dkieffer/stacker/static/css/main.8f9334af.chunk.css"
+  },
+  {
+    "revision": "dbdda07c1a448af17cd0",
+    "url": "/dkieffer/stacker/static/js/2.9dcfd0d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dkieffer/stacker/static/js/2.9dcfd0d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bd0817acb14d1c1e95",
+    "url": "/dkieffer/stacker/static/js/main.f8080cb4.chunk.js"
+  },
+  {
+    "revision": "ab63a54b1ebe5c5c094a",
+    "url": "/dkieffer/stacker/static/js/runtime-main.14600e1a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dkieffer/stacker/static/media/logo.5d5d9eef.svg"
+  }
+]);
