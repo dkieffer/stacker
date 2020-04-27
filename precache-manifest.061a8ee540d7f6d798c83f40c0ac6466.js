dafe05c41b042e07f7ee9d86b97bf678f0e74666
David <david@kiefferbros.com>
2020-04-26 23:03:08 -0500

    Updates

---
+++ precache-manifest.061a8ee540d7f6d798c83f40c0ac6466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559a2748bb336798e5afb0ca84313291",
+    "url": "/stacker/index.html"
+  },
+  {
+    "revision": "45b5692dd83b9ebb239b",
+    "url": "/stacker/static/css/main.8f9334af.chunk.css"
+  },
+  {
+    "revision": "dbdda07c1a448af17cd0",
+    "url": "/stacker/static/js/2.9dcfd0d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stacker/static/js/2.9dcfd0d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b5692dd83b9ebb239b",
+    "url": "/stacker/static/js/main.d2096317.chunk.js"
+  },
+  {
+    "revision": "6aa6d2f56c1102d76ade",
+    "url": "/stacker/static/js/runtime-main.f9d0535d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stacker/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dkieffer/stacker/precache-manifest.5d23da4152ab988331aba4b151106bb9.js"
+  "/stacker/precache-manifest.061a8ee540d7f6d798c83f40c0ac6466.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dkieffer/stacker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stacker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
